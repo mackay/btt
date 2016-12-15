0c2c1072354d56fb50ef247270f426e086364b19
Doug Mackay <mackay.doug@gmail.com>
2016-12-15 08:13:39 -0800

    classification model cleanup

--- btt/classification/models.py
+++ btt/classification/models.py
@@ -53,14 +53,3 @@ class ClassificationMeta(models.Model):
     key = models.CharField(max_length=64)
     text = models.TextField(default="")
     numeric = models.FloatField(default=0.0)
-
-
-# class Question(models.Model):
-#     question_text = models.CharField(max_length=200)
-#     pub_date = models.DateTimeField('date published')
-
-
-# class Choice(models.Model):
-#     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-#     choice_text = models.CharField(max_length=200)
-#     votes = models.IntegerField(default=0)
